89d5fe707db66e6935cab3c14a61c62b50e82a3a
ansehyeong <dkstpgud@gmail.com>
2022-02-10 08:41:09 +0900

    feat: 자바 기본 두번째
    - 프리미티브 데이터 타입
      - char
      - boolean
    - String

---
+++ src/main/java/javas/study/inflearn/not/inflear/udemy/firststep/StringStudy.java
@@ -0,0 +1,15 @@
+package javas.study.inflearn.not.inflear.udemy.firststep;
+
+public class StringStudy {
+    public static void main(StringStudy[] args) {
+        // byte, short, int, long, float, double, char, boolean -> primitive data type
+        // String
+
+        String strValue = "this is string";
+
+        System.out.println("strValue = " + strValue);
+
+        // 자바에서 String 은 불변 타입이다. 불변 타입은 생성된 이후엔 수정이 불가능하며 이미 생성된 문자열을 수정하는 경우 새로운 문자열이 생성된다.
+        
+    }
+}
