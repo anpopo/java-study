c761ca07cf7c3d3f7f6b7a4561b820a5301aad6d
ansehyeong <dkstpgud@gmail.com>
2022-02-17 09:16:18 +0900

    feat: switch / for / while / do while / scanner 학습 및 구현 연습

---
+++ src/main/java/javas/study/inflearn/not/inflear/udemy/thridcontrolflow/ParsingValueFromString.java
@@ -0,0 +1,9 @@
+package javas.study.inflearn.not.inflear.udemy.thridcontrolflow;
+
+public class ParsingValueFromString {
+
+    public static void main(String[] args) {
+        String intValue = "2022";
+        System.out.println("intValue = " + Integer.parseInt(intValue));
+    }
+}
---
+++ src/main/java/javas/study/inflearn/not/inflear/udemy/thridcontrolflow/ReadingUserInput.java
@@ -0,0 +1,30 @@
+package javas.study.inflearn.not.inflear.udemy.thridcontrolflow;
+
+import java.time.LocalDateTime;
+import java.util.Scanner;
+
+public class ReadingUserInput {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter your year of birth : ");
+        boolean hasNextInt = sc.hasNextInt();
+        if (hasNextInt) {
+            int yearOfBirth = sc.nextShort();
+            sc.nextLine();  // 개행 문자 handle
+
+            System.out.print("enter your name : ");
+            String name = sc.nextLine();
+
+            int age = LocalDateTime.now().getYear() - yearOfBirth;
+            if (age >= 0 && age <= 200) {
+                System.out.println("Your name is " + name + ", and you are " + age + " years old");
+            } else {
+                System.out.println("Invalid year of birth");
+            }
+        } else {
+            System.out.println("Unable to parse year of birth");
+        }
+
+        sc.close();
+    }
+}
