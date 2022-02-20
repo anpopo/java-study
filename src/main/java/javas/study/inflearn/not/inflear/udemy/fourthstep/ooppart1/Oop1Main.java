8dbee302e594b17c309be654197652863db7399b
ansehyeong <dkstpgud@gmail.com>
2022-02-20 22:41:11 +0900

    feat: Object-Oriented Programming part 1 started

---
+++ src/main/java/javas/study/inflearn/not/inflear/udemy/fourthstep/ooppart1/Oop1Main.java
@@ -0,0 +1,11 @@
+package javas.study.inflearn.not.inflear.udemy.fourthstep.ooppart1;
+
+public class Oop1Main {
+
+    public static void main(String[] args) {
+
+        Car porsche = new Car();
+        Car holden = new Car();
+        porsche.setModel("Carrera");
+    }
+}
