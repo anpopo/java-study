8ebebedbfd114ccb3d8eae7fd4c2fd6f836a7bf3
ansehyeong <dkstpgud@gmail.com>
2022-02-22 08:35:24 +0900

    feat: class / constructor 학습

--- src/main/java/javas/study/inflearn/not/inflear/udemy/fourthstep/ooppart1/Oop1Main.java
+++ src/main/java/javas/study/inflearn/not/inflear/udemy/fourthstep/ooppart1/Oop1Main.java
@@ -4,8 +4,13 @@ public class Oop1Main {
 
     public static void main(String[] args) {
 
-        Car porsche = new Car();
-        Car holden = new Car();
-        porsche.setModel("Carrera");
+//        Car porsche = new Car();
+//        Car holden = new Car();
+//        porsche.setModel("Carrera");
+
+        Account sehyeongAccount = new Account();
+        sehyeongAccount.deposit(Double.MAX_VALUE);
+
+        sehyeongAccount.withdrawal(1000d);
     }
 }
