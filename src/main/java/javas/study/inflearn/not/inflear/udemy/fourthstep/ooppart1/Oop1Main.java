4e957a2d84308b825d0d8e5d4bdef8c1298273ce
ansehyeong <dkstpgud@gmail.com>
2022-02-23 09:26:17 +0900

    feat: 상속 기본 학습

--- src/main/java/javas/study/inflearn/not/inflear/udemy/fourthstep/ooppart1/Oop1Main.java
+++ src/main/java/javas/study/inflearn/not/inflear/udemy/fourthstep/ooppart1/Oop1Main.java
@@ -8,9 +8,13 @@ public class Oop1Main {
 //        Car holden = new Car();
 //        porsche.setModel("Carrera");
 
-        Account sehyeongAccount = new Account();
-        sehyeongAccount.deposit(Double.MAX_VALUE);
+//        Account sehyeongAccount = new Account();
+//        sehyeongAccount.deposit(Double.MAX_VALUE);
+//        sehyeongAccount.withdrawal(1000d);
 
-        sehyeongAccount.withdrawal(1000d);
+        Animal animal = new Animal("Animal", 1, 1, 5, 5);
+        Dog dog = new Dog("Yorkie", 8, 20, 2, 4, 1, 20, "long silky");
+        dog.eat();
+
     }
 }
