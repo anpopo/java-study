378e4b3dd6c631351b661f10de9d68125aae30d9
ansehyeong <dkstpgud@gmail.com>
2022-03-07 08:32:41 +0900

    feat: array 연습
    - array 사용시 가장 많이 발생하는 에러 3가지
      - 인덱스가 존재하지 않는 경우 ArrayOutOfBoundException
      - for loop 시 1부터 시작
      - for loop 시 마지막 인덱스보다 큰 값을 포함

--- src/main/java/javas/study/inflearn/not/inflearn/udemy/stepfive/ArraysStudy.java
+++ src/main/java/javas/study/inflearn/not/inflearn/udemy/stepfive/ArraysStudy.java
@@ -1,17 +1,60 @@
 package javas.study.inflearn.not.inflearn.udemy.stepfive;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class ArraysStudy {
+    private static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-        int[] intArray = new int[10];
 
-        intArray[5] = 15;
-        intArray[2] = 12;
 
-        int[] intArrays = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int[] intArrays = getIntegers();
 
-        for (int i = 0; i < intArrays.length; i++) {
-            System.out.println(intArrays[i]);
+        printArray(intArrays);
+
+        int[] sortedArray = sortIntegers(intArrays);
+
+        printArray(sortedArray);
+
+    }
+
+    private static int[] getIntegers() {
+        int[] array = new int[sc.nextInt()];
+        System.out.println("enter your number");
+        for (int i = 0; i< array.length; i++) {
+            array[i] = sc.nextInt();
         }
+        return array;
+    }
+
+    public static void printArray(int[] array) {
+        for (int i = 0; i < array.length; i ++) {
+            System.out.println("element " + i + " = " + array[i]);
+        }
+    }
+
+    public static int[] sortIntegers(int[] array) {
+//        int[] sortedArray = new int[array.length];
+//
+//        for (int i = 0; i < array.length; i++) {
+//            sortedArray[i] = array[i];
+//        }
+        int[] sortedArray = Arrays.copyOf(array, array.length);
+        boolean flag = true;
+
+        int temp;
+        while (flag) {
+            flag = false;
+            for (int i = 0; i < sortedArray.length - 1; i++) {
+                if (sortedArray[i] < sortedArray[i + 1]) {
+                    temp = sortedArray[i];
+                    sortedArray[i] = sortedArray[i + 1];
+                    sortedArray[i + 1] = temp;
+                    flag = true;
+                }
+            }
+        }
+        return sortedArray;
     }
 }
