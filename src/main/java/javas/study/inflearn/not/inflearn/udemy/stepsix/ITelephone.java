b65950f07ee68686d21f8a01cddba940fd0be6f3
ansehyeong <dkstpgud@gmail.com>
2022-03-14 06:57:13 +0900

    feat: Interface 기본, 연습

---
+++ src/main/java/javas/study/inflearn/not/inflearn/udemy/stepsix/ITelephone.java
@@ -0,0 +1,14 @@
+package javas.study.inflearn.not.inflearn.udemy.stepsix;
+
+public interface ITelephone {
+    void powerOn();
+
+    void dial(int phoneNumber);
+
+    void answer();
+
+    boolean callPhone(int phoneNumber);
+
+    boolean isRinging();
+
+}
