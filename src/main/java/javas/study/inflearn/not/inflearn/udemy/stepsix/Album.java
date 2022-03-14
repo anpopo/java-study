5251bf8ee5a8eedb4b02d4668a84765e2c8d094e
ansehyeong <dkstpgud@gmail.com>
2022-03-14 09:00:46 +0900

    feat: inner class 기본 & 연습

--- src/main/java/javas/study/inflearn/not/inflearn/udemy/stepsix/Album.java
+++ src/main/java/javas/study/inflearn/not/inflearn/udemy/stepsix/Album.java
@@ -1,49 +1,62 @@
 package javas.study.inflearn.not.inflearn.udemy.stepsix;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 
 public class Album {
 
     private String name;
     private String artist;
-    private ArrayList<Song> songs;
+    private SongList songs;
 
     public Album(String name, String artist) {
         this.name = name;
         this.artist = artist;
-        songs = new ArrayList<>();
+        songs = new SongList();
     }
 
     public boolean addSong(String title, double duration) {
-        if (findSong(title) == null) {
-            return songs.add(new Song(title, duration));
-        }
-        return false;
+        return this.songs.add(new Song(title, duration));
     }
-
-    private Song findSong(String title) {
-        for (Song song : songs) {
-            if (song.getTitle().equals(title)) return song;
-        }
-        return null;
-    }
-
     public boolean addToPlayList(int trackNumber, List<Song> playList) {
-        int index = trackNumber - 1;
-        if (index >= 0 && (index <= this.songs.size())) {
-            return playList.add(this.songs.get(index));
-        }
+        Song checkedSong = this.songs.findSong(trackNumber);
+        if (checkedSong != null) return playList.add(checkedSong);
         return false;
     }
 
     public boolean addToPlayList(String title, List<Song> playList) {
-        Song song = findSong(title);
-
-        if (song != null) {
-            return playList.add(song);
-        }
+        Song song = songs.findSong(title);
+        if (song != null) return playList.add(song);
         return false;
+    }
+
+    private class SongList {
+        private List<Song> songs;
+
+        public SongList() {
+            this.songs = new ArrayList<>();
+        }
+
+        public boolean add (Song song) {
+            if (songs.contains(song)) {
+                return false;
+            }
+            return songs.add(song);
+        }
+
+        private Song findSong(String title) {
+            for (Song song : songs) {
+                if (song.getTitle().equals(title)) return song;
+            }
+            return null;
+        }
+
+        public Song findSong(int trackNumber) {
+            int index = trackNumber -1;
+            if (index >= 0 && index < songs.size()) {
+                return songs.get(index);
+            }
+            return null;
+        }
     }
 }
