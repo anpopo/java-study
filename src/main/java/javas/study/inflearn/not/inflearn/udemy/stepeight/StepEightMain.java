26732dcb76d252b6ed4ecfee8effbe0e31934612
ansehyeong <dkstpgud@gmail.com>
2022-03-21 09:15:27 +0900

    feat: naming convention, package

---
+++ src/main/java/javas/study/inflearn/not/inflearn/udemy/stepeight/StepEightMain.java
@@ -0,0 +1,9 @@
+package javas.study.inflearn.not.inflearn.udemy.stepeight;
+
+public class StepEightMain {
+
+    public static void main(String[] args) {
+        MyWindow myWindow = new MyWindow("Complete Java");
+        myWindow.setVisible(true);
+    }
+}
