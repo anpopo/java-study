93413173f67ac729bbfd8897ff9fa72449134683
ansehyeong <dkstpgud@gmail.com>
2022-03-29 09:02:34 +0900

    feat: Basic of Map - key, value 구조를 가지는 자료구조

---
+++ src/main/java/javas/study/inflearn/not/inflearn/udemy/stepnine/MapPractice.java
@@ -0,0 +1,34 @@
+package javas.study.inflearn.not.inflearn.udemy.stepnine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapPractice {
+    public static void main(String[] args) {
+        Map<String, String> languages = new HashMap<>();
+        languages.put("java", "compiled high level, object-oriented, platform independent language");
+        languages.put("python", "interpreted, object-oriented, high-level programming language with dynamic semantics");
+        languages.put("algol", "algorithmic language");
+        languages.put("basic", "beginners all purposes symbolic instruction code");
+        languages.put("lips", "therein lies madness");
+
+
+
+        System.out.println(languages.get("Java"));
+        languages.put("java", "this course is about java");
+        System.out.println(languages.get("java"));
+
+
+
+        if (languages.containsKey("java")) {
+            System.out.println("java is already in the map");
+        }
+
+        System.out.println("=====================================================================================================================================");
+
+        for (String key : languages.keySet()) {
+            System.out.println(key + " : " + languages.get(key));
+        }
+
+    }
+}
