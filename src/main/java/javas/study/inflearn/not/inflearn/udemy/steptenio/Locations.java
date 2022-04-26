39c394c6234077a02c18cb5e30099929043e9b6a
ansehyeong <dkstpgud@gmail.com>
2022-04-27 08:37:36 +0900

    feat: try-with-resource statement  / FileWriter & FileReader

--- src/main/java/javas/study/inflearn/not/inflearn/udemy/steptenio/Locations.java
+++ src/main/java/javas/study/inflearn/not/inflearn/udemy/steptenio/Locations.java
@@ -1,67 +1,97 @@
 package javas.study.inflearn.not.inflearn.udemy.steptenio;
 
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class Locations implements Map<Integer, Location> {
 
     private static Map<Integer, Location> locations = new HashMap<>();
 
-    public static void main(String[] args) {
-        FileWriter locationFile = null;
-        try {
-            locationFile = new FileWriter("locations.txt");
+    public static void main(String[] args) throws IOException {
+//        FileWriter locationFile = null;
+//        try {
+//            locationFile = new FileWriter("locations.txt");
+//
+//            for (Location location : locations.values()) {
+//                locationFile.write(location.getLocationId() + ", " + location.getDescription() + "\n");
+//            }
+//
+//        } finally {
+//            if (locationFile != null) {
+//                locationFile.close();
+//            }
+//        }
+
+
+
+        try (FileWriter locationFile = new FileWriter("locations.txt");
+             FileWriter directionFIle = new FileWriter("directions.txt")
+        ) {
 
             for (Location location : locations.values()) {
                 locationFile.write(location.getLocationId() + ", " + location.getDescription() + "\n");
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                if (locationFile != null) {
-                    locationFile.close();
-                }
-            } catch (IOException e) {
 
+                for (String direction : location.getExits().keySet()) {
+                    directionFIle.write(location.getLocationId() + ", " + direction + ", + location.getExits().get(direction)" + "\n");
+                }
             }
         }
-
     }
 
     static {
-        Map<String, Integer> tempExit = new HashMap<>();
-        locations.put(0, new javas.study.inflearn.not.inflearn.udemy.steptenio.Location(0, "You are sitting in front of a computer learning Java", tempExit));
 
-        tempExit = new HashMap<>();
-        tempExit.put("W", 2);
-        tempExit.put("E", 3);
-        tempExit.put("S", 4);
-        tempExit.put("N", 5);
-        locations.put(1, new javas.study.inflearn.not.inflearn.udemy.steptenio.Location(1, "You are standing at the end of a road before a small bridge", tempExit));
+        Scanner sc = null;
+        try {
+            sc = new Scanner(new FileReader("locations.txt"));
+            sc.useDelimiter(",");
 
-        tempExit = new HashMap<>();
-        tempExit.put("N", 5);
-        locations.put(2, new javas.study.inflearn.not.inflearn.udemy.steptenio.Location(2, "You are at the top of a hill", tempExit));
+            while (sc.hasNextLine()) {
+                int loc = sc.nextInt();
+                sc.skip(sc.delimiter());
+                String description = sc.nextLine();
+                System.out.println("Imported loc : " + loc + ": " + description);
 
-        tempExit = new HashMap<>();
-        tempExit.put("W", 1);
-        locations.put(3, new javas.study.inflearn.not.inflearn.udemy.steptenio.Location(3, "You are inside a building, a well house for a small spring", tempExit));
+                Map<String, Integer> tempExits = new HashMap<>();
+                locations.put(loc, new Location(loc, description, tempExits));
+            }
+        } catch (IOException e) {
 
-        tempExit = new HashMap<>();
-        tempExit.put("N", 1);
-        tempExit.put("W", 2);
-        locations.put(4, new javas.study.inflearn.not.inflearn.udemy.steptenio.Location(4, "You are in a valley beside a stream", tempExit));
+        } finally {
+            if (sc != null){
+                sc.close();
+            }
+        }
 
-
-        tempExit = new HashMap<>();
-        tempExit.put("S", 1);
-        tempExit.put("W", 2);
-        locations.put(5, new javas.study.inflearn.not.inflearn.udemy.steptenio.Location(5, "You are in the forest", tempExit));
+//        Map<String, Integer> tempExit = new HashMap<>();
+//        locations.put(0, new javas.study.inflearn.not.inflearn.udemy.steptenio.Location(0, "You are sitting in front of a computer learning Java", tempExit));
+//
+//        tempExit = new HashMap<>();
+//        tempExit.put("W", 2);
+//        tempExit.put("E", 3);
+//        tempExit.put("S", 4);
+//        tempExit.put("N", 5);
+//        locations.put(1, new javas.study.inflearn.not.inflearn.udemy.steptenio.Location(1, "You are standing at the end of a road before a small bridge", tempExit));
+//
+//        tempExit = new HashMap<>();
+//        tempExit.put("N", 5);
+//        locations.put(2, new javas.study.inflearn.not.inflearn.udemy.steptenio.Location(2, "You are at the top of a hill", tempExit));
+//
+//        tempExit = new HashMap<>();
+//        tempExit.put("W", 1);
+//        locations.put(3, new javas.study.inflearn.not.inflearn.udemy.steptenio.Location(3, "You are inside a building, a well house for a small spring", tempExit));
+//
+//        tempExit = new HashMap<>();
+//        tempExit.put("N", 1);
+//        tempExit.put("W", 2);
+//        locations.put(4, new javas.study.inflearn.not.inflearn.udemy.steptenio.Location(4, "You are in a valley beside a stream", tempExit));
+//
+//
+//        tempExit = new HashMap<>();
+//        tempExit.put("S", 1);
+//        tempExit.put("W", 2);
+//        locations.put(5, new javas.study.inflearn.not.inflearn.udemy.steptenio.Location(5, "You are in the forest", tempExit));
     }
 
     @Override
